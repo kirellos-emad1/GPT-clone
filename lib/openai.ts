50053e81a9423da1f0bcebc864509c5e22599a82
Kirellos Wasfy <94471342+kirellos-emad1@users.noreply.github.com>
2025-02-06 04:43:58 +0200

    update the openAI API version

--- lib/openai.ts
+++ lib/openai.ts
@@ -1,7 +1,7 @@
 import { AzureOpenAI } from "openai";
 const endpoint = process.env.AZURE_OPENAI_ENDPOINT || "https://gpt-clone.openai.azure.com/";  
 const apiKey = process.env.AZURE_OPENAI_API_KEY ;  
-const apiVersion = "2024-05-01-preview";  
+const apiVersion = "2024-08-01-preview";  
 const deployment = "gpt-35-turbo"
 const embeddingsDeployment = "text-embedding-ada-002"; // Your embedding deployment name
 
